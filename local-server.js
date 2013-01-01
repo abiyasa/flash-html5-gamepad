44e6a62f6d6d563b34a87870dca2c31268c62573
abiyasa <ilham.suhardi@gmail.com>
2013-01-01 18:14:25 +0100

    add a simple webserver using node.js + connect

---
+++ local-server.js
@@ -0,0 +1,11 @@
+/**
+ * A Simple local webserver using node.js & connect
+ *
+ * How to install: npm install connect
+ * How to run: node local-server.js
+ */
+var connect = require('connect');
+var server = connect()
+  .use(connect.static(__dirname))
+  .use(connect.logger('dev'))
+  .listen(9000);
